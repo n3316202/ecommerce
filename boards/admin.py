118e634f1b62c370a7ffc86a09f24cd800244dfd
TEACHER\ICT02_22 <admin@admin.com>
2024-12-04 14:38:33 +0900

    관리자

--- boards/admin.py
+++ boards/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
 from django.contrib import admin
 from .models import Comment, Post
 
+# Register your models here.
+
+#admin.site.register(Post)
+
+
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ['title','content']
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
-admin.site.register(Post)
