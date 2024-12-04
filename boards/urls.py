4aaea1992e64c963807e113383aab46b489dbf30
TEACHER\ICT02_22 <admin@admin.com>
2024-12-04 16:55:06 +0900

    템플릿 문법

--- boards/urls.py
+++ boards/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
    path("", views.index), #boards/ 끝에 슬러시 주의 할것
+   path("for_loop/", views.for_loop), #boards/for_loop/ 끝에 슬러시 주의 할것
 ]
--- boards/views.py
+++ boards/views.py
@@ -8,3 +8,12 @@ def index(request):
     post_list = Post.objects.order_by('-created_at') 
     context = {'post_list': post_list}
     return render(request, 'boards/post_list.html', context)
+
+def for_loop(request):
+    post_list = Post.objects.all
+    #context = {'post_list': post_list}
+    #추가
+    name = {'name':'jaewon', 'age':99, 'location':'seoul', 'isman':True}
+    price = [100,200,300]
+
+    return render(request, 'boards/for_loop.html', {'post_list': post_list,'name' : name, 'price':price})
