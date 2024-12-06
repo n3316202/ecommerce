0d13468150d561873f0f7bd8ea5761d32a472c93
heo <admin@admin.com>
2024-12-06 19:16:09 +0900

    완료

--- boards/forms.py
+++ boards/forms.py
@@ -1,7 +1,23 @@
 from django import forms
-from boards.models import Post
+from boards.models import Comment, Post
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post  # 사용할 모델
-        fields = ['title', 'content'] 
+        fields = ['title', 'content']
+        # widgets = {
+        #     'subject': forms.TextInput(attrs={'class': 'form-control'}),
+        #     'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 10}),
+        # } 
+        labels = {
+            'title': '제목',
+            'content': '내용',
+        } 
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment  # 사용할 모델
+        fields = ['content']
+        labels = {
+            'content': '댓글',
+        }   
--- boards/views.py
+++ boards/views.py
@@ -1,8 +1,9 @@
+from django.http import HttpResponseNotAllowed
 from django.utils import timezone   
 from django.shortcuts import get_object_or_404, redirect, render
 
-from boards.forms import PostForm
-from boards.models import Post
+from boards.forms import CommentForm, PostForm
+from boards.models import Comment, Post
 
 # Create your views here.
 # -기호가 붙으면 역방향 정렬을, 없으면 순방향 정렬을 의미한다. 게시물은 보통 최신순으로 보므로 작성일시를 역순으로 정렬했다.
@@ -45,4 +46,24 @@ def post_create(request):
         form = PostForm()
 
     context = {'form': form}
+    return render(request, 'boards/post_form.html', context)
+
+def comment_create(request,post_id):
+    """
+    댓글 등록
+    """
+    post = get_object_or_404(Post, pk=post_id)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        
+        if form.is_valid(): # 폼이 유효하다면
+            comment = form.save(commit=False) # 임시 저장하여 post 객체를 리턴받는다.
+            #post.created_at = timezone.now()# 실제 저장을 위해 작성일시를 설정한다.
+            comment.post = post
+            comment.save()# 데이터를 실제로 저장한다.
+            return redirect('boards:index', post_id=post.id)
+    else:
+        return HttpResponseNotAllowed('Only POST is possible.')
+
+    context = {'post':post,'form': form}
     return render(request, 'boards/post_form.html', context)
