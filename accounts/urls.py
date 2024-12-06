c05c330a334c1e617d48d20fa9f8698631680719
heo <admin@admin.com>
2024-12-07 00:38:39 +0900

    로그인-로그아웃

---
+++ accounts/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import include, path
+
+from django.contrib.auth import views as auth_views
+
+from accounts import views
+
+app_name = 'accounts'
+
+urlpatterns = [
+     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'), 
+     path('logout/', views.logout_view, name='logout'),
+]
--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'home',
     'boards',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# 로그인 성공후 이동하는 URL
+LOGIN_REDIRECT_URL = '/'
