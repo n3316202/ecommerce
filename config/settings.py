490dedbb9db29c5e163e68b85d5cff81afb362fd
heo <admin@admin.com>
2024-12-08 13:55:29 +0900

    완료

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'home',
     'boards',
     'accounts',
+    'store'
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -3,8 +3,8 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include('home.urls'),name='home'),
+    path("", include('store.urls')),
     path("home/", include('home.urls')),
     path("boards/", include('boards.urls')),
-    path('account/', include('accounts.urls')),    
+    path('account/', include('accounts.urls')),
 ]
