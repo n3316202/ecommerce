db308e334bc612d5f8248700b3fdd68787b11732
heo <admin@admin.com>
2024-12-08 21:57:39 +0900

    commit

--- store/views.py
+++ store/views.py
@@ -7,3 +7,6 @@ from store.models import Product
 def home(request):
     products = Product.objects.all()
     return render(request,'store/home.html',{'products':products})
+
+def about(request):
+    return render(request,'store/about.html',{})
