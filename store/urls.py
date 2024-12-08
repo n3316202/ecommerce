d311666fdcb0441b13999f67f215880cf8d0bcb4
heo <admin@admin.com>
2024-12-09 02:17:36 +0900

    commit

--- store/urls.py
+++ store/urls.py
@@ -6,5 +6,6 @@ app_name = 'store'
 urlpatterns = [
     path('', views.home, name='home'),  
     path('about/', views.about, name='about'),
-    path('product/<int:product_id>', views.product, name='product'),    
+    path('product/<int:product_id>', views.product, name='product'),
+    path('category/<str:foo>', views.category, name='category'), #변수 foo 의 의미  참고:https://namu.wiki/w/foo
 ]
--- store/views.py
+++ store/views.py
@@ -1,12 +1,15 @@
+from calendar import c
 from itertools import product
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-from store.models import Product
+from store.models import Category, Product
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     products = Product.objects.all()
-    return render(request,'store/home.html',{'products':products})
+    categories = Category.objects.all()
+    return render(request,'store/home.html',{'products':products,'categories':categories})
 
 def about(request):
     return render(request,'store/about.html',{})
@@ -14,3 +17,24 @@ def about(request):
 def product(request,product_id):
     product = Product.objects.get(id=product_id)
     return render(request, 'store/product.html', {'product':product})
+
+def category(request, foo):
+    #Replace Hyphens with Spaces
+
+    foo = foo.replace('-',' ')
+    print(foo)
+
+    # Grab the category from the url
+    try:
+        #Look up the category
+        category = Category.objects.get(name=foo)
+        print(category)
+        
+        products = Product.objects.filter(category=category)
+        print(products)
+        
+        return render(request, 'store/category.html',{'products':products,'category':category})
+    
+    except:
+        messages.success(request, ("카테고리가 존재하지 않습니다."))        
+        return redirect('store:home')
