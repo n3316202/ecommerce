becfd91dba42037fa58ecf0c7df2c10993097e6c
heo <admin@admin.com>
2024-12-08 15:08:01 +0900

    모델 완성

--- store/models.py
+++ store/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(default=0,decimal_places=2,max_digits=6) #9999.99
+    category = models.ForeignKey(Category,on_delete=models.CASCADE,default=1)
+    description = models.CharField(max_length=250,default='',blank=True, null=True)
+    image = models.ImageField(upload_to='upload/product')
+
+    def __str__(self):
+        return self.name
+
+#User orders
+class Order(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    customer = models.ForeignKey(User,on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    address = models.CharField(max_length=100, default='', blank=True)
+    phone = models.CharField(max_length=20,default='',blank=True)
+    status = models.BooleanField(default = False)
+    created_at  = models.DateTimeField(auto_now_add=True)
+    updated_at  = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.product
