36120c096af2f32b89ed74b4d296bcfa5bc331f8
heo <admin@admin.com>
2025-04-06 00:13:25 +0900

    commit

--- api/views.py
+++ api/views.py
@@ -128,9 +128,9 @@ class CategoryViewSet(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+# dev_55 아래 주석처리
 
 # REST API 규격에 맞춘 URL 매핑
-
 # List Route
 # category_list = CategoryViewSet.as_view({
 #     'get': 'list',
